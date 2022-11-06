c3a4429a07dc0d94edfe234f5e6b5dd12c41a134
JPalacio <Joelpalacio630@gmail.com>
2022-11-05 20:17:10 -0600

    Javascript a la ventana modal para agregar una nueva tarea en el proyecto

---
+++ src/js/tareas.js
@@ -0,0 +1,42 @@
+(function () {
+    // Boton para mostrar el Modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click',mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+<form class="formulario nueva-tarea">
+<legend>Añade una Nueva Tarea</legend>
+<div class="campo">
+<label>Tarea: </label>
+<input
+type="text"
+name="tarea"
+placeholder="Añadir Tarea al Proyecto Actual"
+id="tarea"
+/>
+</div>
+
+<div class="opciones">
+<input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
+<button type="button" class="cerrar-modal">Cancelar</button>
+
+
+</div>
+
+</form>
+`;
+
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+
+        },100);
+
+
+        document.querySelector('body').appendChild(modal);
+
+    }
+})();
